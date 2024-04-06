0fe1fe7111b8a7e9fa07313fe71fcbb7141a70e8
Amanda Cantiliano Idalgo <amandalavigne2@gmail.com>
2024-04-05 21:33:38 -0300

    login usando firebase

---
+++ Autenticacao/firebase.js
@@ -0,0 +1,32 @@
+// Import the functions you need from the SDKs you need
+//import * as firebase from "firebase
+//import * as firebase from "firebase/app";
+import firebase from "firebase/compat/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// cada produto do firebase deve ser importado separadamente
+//por exemplo auth de autenticação
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCklw6NR76-czP6qpL12EafdQExJBWBIxg",
+  authDomain: "tads-950e1.firebaseapp.com",
+  projectId: "tads-950e1",
+  storageBucket: "tads-950e1.appspot.com",
+  messagingSenderId: "1052069800360",
+  appId: "1:1052069800360:web:d37727bb6f85d4b6d9559d"
+};
+
+// Initialize Firebase
+let app;
+if (firebase.apps.length === 0) {
+  app = firebase.initializeApp(firebaseConfig);
+} else {
+  app = firebase.app()
+}
+
+const auth = firebase.auth()
+const firestore = firebase.firestore()
+export { auth, firestore };
